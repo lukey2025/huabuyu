875f28df5b34ae5532f3462910bc0d10209c9305
lukey2025 <lukey8658@gmail.com>
2025-11-21 17:33:21 +0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,16 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+// API utility functions
+export async function handleApiError(response: Response) {
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message || 'API request failed');
+  }
+  return response.json();
+}
 
